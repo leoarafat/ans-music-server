711d149e8558100191581f47821cc341258e2ee5
leoarafat <leoarafat10@gmail.com>
2024-04-01 17:33:35 +0600

    change manage api

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,12 @@ export const app: Application = express();
 //cors
 app.use(
   cors({
-    origin: ['*'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://127.0.0.1:5174',
+    ],
     credentials: true,
   }),
 );
