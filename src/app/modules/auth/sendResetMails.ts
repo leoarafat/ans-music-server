fd4444c0bd76d358920128747bb73fefaa8c04c1
leoarafat <leoarafat10@gmail.com>
2024-04-07 13:18:28 +0600

    delete test folder

--- src/app/modules/auth/sendResetMails.ts
+++ src/app/modules/auth/sendResetMails.ts
@@ -1,21 +1,21 @@
-// import nodemailer from 'nodemailer';
-// import config from '../../../config';
+import nodemailer from 'nodemailer';
+import config from '../../../config';
 
-// export async function sendEmail(to: string, html: string) {
-//   const transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port: 587,
-//     secure: false,
-//     auth: {
-//       user: config.smtp.smtp_mail,
-//       pass: config.smtp.smtp_password,
-//     },
-//   });
+export async function sendEmail(to: string, html: string) {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: config.smtp.smtp_mail,
+      pass: config.smtp.smtp_password,
+    },
+  });
 
-//   await transporter.sendMail({
-//     from: config.smtp.smtp_mail,
-//     to,
-//     subject: 'Reset Password Link',
-//     html,
-//   });
-// }
+  await transporter.sendMail({
+    from: config.smtp.smtp_mail,
+    to,
+    subject: 'Reset Password Link',
+    html,
+  });
+}
