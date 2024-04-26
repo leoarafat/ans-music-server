13003d4896445783ea5068b178e1176e96cde557
leoarafat <leoarafat10@gmail.com>
2024-04-26 18:00:37 +0600

    modify album upload

--- src/app/modules/album/album.model.ts
+++ src/app/modules/album/album.model.ts
@@ -233,12 +233,9 @@ const multipleMusicSchema = new Schema<IAlbumMusic>(
       required: true,
     },
 
-    labelName: {
-      type: String,
-      required: true,
-    },
-    labelId: {
-      type: String,
+    label: {
+      type: Schema.Types.ObjectId,
+      ref: 'Label',
       required: true,
     },
 
@@ -322,115 +319,5 @@ const multipleMusicSchema = new Schema<IAlbumMusic>(
     },
   },
 );
-// const multipleMusicSchema = new Schema<IAlbumMusic>(
-//   {
-//     audio: [
-//       {
-//         path: {
-//           type: String,
-//           required: true,
-//         },
-//         title: {
-//           type: String,
-//           required: true,
-//         },
-//         artist: {
-//           type: String,
-//           required: true,
-//         },
-//       },
-//     ],
-
-//     image: {
-//       type: String,
-//       required: true,
-//     },
-
-//     status: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     user: {
-//       type: Schema.Types.ObjectId,
-//       ref: 'user',
-//       required: true,
-//     },
-
-//     trackType: {
-//       type: String,
-//       enum: ['music', 'classic-music', 'jazz-music'],
-//       required: true,
-//     },
-
-//     isRelease: {
-//       type: String,
-//       enum: ['yes', 'no'],
-//       required: true,
-//     },
-
-//     instrumental: {
-//       type: String,
-//       enum: ['yes', 'no'],
-//       required: true,
-//     },
-
-//     secondaryTrackType: {
-//       type: String,
-//       enum: ['original', 'karaoke', 'melody', 'cover', 'cover-by-band'],
-//       required: true,
-//     },
-
-//     primaryArtist: {
-//       type: [String],
-//       required: true,
-//     },
-
-//     primaryArtistSpotifyId: {
-//       type: String,
-//       required: true,
-//     },
-
-//     tiktokStartInSecond: {
-//       type: String,
-//       required: true,
-//     },
-
-//     trackLanguage: {
-//       type: String,
-//       required: true,
-//     },
-
-//     releaseDate: {
-//       type: String,
-//       required: true,
-//     },
-
-//     isAdvancePurchase: {
-//       type: Boolean,
-//       required: true,
-//     },
-
-//     advancePurchaseDate: {
-//       type: String,
-//     },
-//     isApproved: {
-//       type: String,
-//       enum: ['approved', 'rejected', 'pending'],
-//       default: 'pending',
-//     },
-//     correctionNote: {
-//       type: [String],
-//     },
-//     tackDown: {
-//       type: String,
-//     },
-//   },
-//   {
-//     timestamps: true,
-//     toJSON: {
-//       virtuals: true,
-//     },
-//   },
-// );
 
 export const Album = model('Album', multipleMusicSchema);
