73c4a773fc85b351823c473ac3d034d70a505245
leoarafat <leoarafat10@gmail.com>
2024-04-27 11:16:59 +0600

    upload build file and modify script

--- dist/constants/pagination.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const paginationFields = ['page', 'limit', 'sortBy', 'sortOrder'];
-exports.default = paginationFields;
