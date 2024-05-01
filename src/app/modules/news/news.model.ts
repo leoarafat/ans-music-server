47e4b3b7b5718a7e291d4255d5aa2db2da068711
leoarafat <leoarafat10@gmail.com>
2024-05-01 10:59:14 -0400

    update new model

--- src/app/modules/news/news.model.ts
+++ src/app/modules/news/news.model.ts
@@ -1,14 +1,22 @@
 import { Schema, model } from 'mongoose';
 import { INews } from './news.interface';
-const newsSchema = new Schema<INews>({
-  title: {
-    type: String,
-    required: true,
+const newsSchema = new Schema<INews>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
   },
-});
+);
 
 export const News = model('News', newsSchema);
